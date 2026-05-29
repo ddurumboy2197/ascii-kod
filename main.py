24d4e7907eff7c2d76dafac4e4be0b2f3614cbf7
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:23:41 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,5 @@
+def belgining_ascii_kodi(belgi):
+    return ord(belgi)
+
+belgi = input("Belgining ASCII kodini chiqarish uchun belginizni kiriting: ")
+print(f"Belgining ASCII kodi: {belgining_ascii_kodi(belgi)}")
